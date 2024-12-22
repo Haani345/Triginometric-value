e32054e942bdb564c4813c3d6e6ad5a9dcdc07f0
Haani345 <std_16-409@generations.edu.pk>
2024-12-22 13:28:54 +0500

    Triginometric value

---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+
+num1=int(input("Enter the number for sin: "))
+num2=int(input("Enter the number for cos: "))
+num3=int(input("Enter the number for tan: "))
+
+print(math.sin(num1))
+print(math.cos(num2))
+print(math.tan(num3))
